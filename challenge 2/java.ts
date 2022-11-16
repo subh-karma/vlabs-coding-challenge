ab22712b4694b9d9c62fa9019ae1ed1f9034d92d
subhasish karmakar <75238941+notoriousgreek@users.noreply.github.com>
2022-11-16 18:06:43 +0530

    pc
    complete_merge_file.html can be run directly

---
+++ challenge 2/java.ts
@@ -0,0 +1,11 @@
+var dragValue;
+
+window.onload=function(){
+        var canvas=document.getElementById('myCanvas');
+        var ctx=canvas.getContext("2d");
+        var img=document.getElementById('xx');
+        ctx.drawImage(img,430,600);
+        }
+        
+        
+ 
